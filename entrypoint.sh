2b18bdc459b903444904c0c0848558650cef5a9f
Matt Bentley <mbentley@mbentley.net>
2022-08-05 23:08:23 -0400

    Enhance entrypoint
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,24 @@ echo "Starting airsonic initialization."
 
 if [ ! -d /data/transcode ]
 then
-  echo "INFO - Directory /data/transcode doesn't exist; creating..."
+  echo "INFO - directory /data/transcode doesn't exist; creating..."
   mkdir /data/transcode
   cd /data/transcode
-
-  # make sure symlinks exist for binaries
-  for BIN in ffmpeg lame
-  do
-    if [ ! -L "/usr/bin/${BIN}" ]
-    then
-      echo "INFO - Symlink for ${BIN} doesn't exist; creating..."
-      ln -s "/usr/bin/${BIN}" .
-    fi
-  done
 else
   echo "INFO - found directory /data/transcode"
 fi
+
+# make sure symlinks exist for binaries
+for BIN in ffmpeg lame
+do
+  if [ ! -L "/data/transcode/${BIN}" ]
+  then
+    echo "INFO - symlink for ${BIN} doesn't exist; creating..."
+    ln -s "/usr/bin/${BIN}" "/data/transcode/${BIN}"
+  else
+    echo "INFO - found symlink for ${BIN}"
+  fi
+done
 
 echo "airsonic initialization complete!"
 echo "======================================";echo
